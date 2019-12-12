49e91f549ef92699956595d9c1611336023d5a4c
Fikayo <fik4christ@yahoo.com>
2019-12-12 12:12:47 +0100

    CircleCI Configuration file added

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\User;
+
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    use WithFaker;
+
+    
+    private $password = "mypassword";
+    
+    public function testUserCreation()
+    {
+       	
+       	$name = $this->faker->name();
+       	$email = $this->faker->email();
+
+        $response = $this->postJson('/api/auth/signup', [
+            'name' => $name, 
+            'email' => $email,
+            'password' => $this->password, 
+            'password_confirmation' => $this->password
+        ]); 
+
+
+        $response
+            ->assertStatus(201)
+            ->assertExactJson([
+                'message' => "Successfully created user!",
+            ]);
+    }//testUserCreation
+
+    
+    
+}
